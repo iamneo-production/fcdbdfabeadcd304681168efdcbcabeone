cd7d840e37617144de299d7b65f4e9b4554aefe3
Iamneo <techadmin@iamneo.ai>
2023-09-03 17:08:15 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -16,7 +16,19 @@ function handleMove(event) {
             endGame();
         } else{
             currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-            RE
+            resultContainer.textContent=`Player ${currentPlayer}'s Turn`;
         }
     }
+}
+
+board.forEach(cell => cell.addEventListener('click',handleMove));
+
+function checkWin(){
+    const winningCombos = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ];
+
+    return winningCombos.some
 }
