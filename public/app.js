d9c6d4e7bd3b77137583148f7ca83df99c78c454
Iamneo <techadmin@iamneo.ai>
2023-09-03 17:03:13 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,3 +1,22 @@
 const board = document.querySelectorAll('.btn');
-const resultContainer = document.querySelector(''.result);
-const resertButton = document.getElementById('reset-button');
+const resultContainer = document.querySelector('.result');
+const resetButton = document.getElementById('reset-button');
+
+let currentPlayer = 'X';
+let isGameOver = false;
+
+function handleMove(event) {
+    const cell =event.target;
+
+    if(!cell.value && !isGameOver){
+        cell.value = currentPlayer;
+        cell.classList.add(currentPlayer);
+
+        if(checkWin() || checkDraw()){
+            endGame();
+        } else{
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            RE
+        }
+    }
+}
